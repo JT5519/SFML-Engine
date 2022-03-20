7500c64487e17f6de1377d40270b527caba460b6
JT5519 <joel.sk10@gmail.com>
2022-03-19 23:44:08 -0400

    Minor Cleanup

--- Engine/Engine/SceneGame.cpp
+++ Engine/Engine/SceneGame.cpp
@@ -82,9 +82,6 @@ void SceneGame::OnCreate()
 
     objects.Add(player);
 
-    // You will need to play around with this offset until it fits the level in at your chosen resolution. This worls for 1920 * 1080.
-    // In future we will remove this hardcoded offset when we look at allowing the player to change resolutions.
-
     sf::Vector2i mapOffset(-160, 180);
     //sf::Vector2i mapOffset(128, 128);
     std::vector<std::shared_ptr<Object>> levelTiles = mapParser.Parse(workingDir.Get() + "Test Map 1.tmx", mapOffset);
